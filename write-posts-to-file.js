14336ce9b6bb1cdc48e9e274ee58f6a18eea8192
Ian Sheridan <ian.sheridan@gmail.com>
2011-05-09 18:15:09 -0400

    now using async.series

--- write-posts-to-file.js
+++ write-posts-to-file.js
@@ -4,33 +4,37 @@ var async= require('async');
 var u = require('util');
 var logger = require('logger').create();
 logger.level = 1;
-var events = require("events").EventEmitter;
 
 // open connection to the CouchDB Blog database
-var Blog = CouchClient("http://localhost:5984/blog");
+var Blog = CouchClient("http://127.0.0.1:5984/blog");
 
 // open a file to write to
-var jsonFile = fs.createWriteStream('posts.json', {'flags': 'a'});
+var jsonFile = fs.createWriteStream('posts.json', {'flags': 'w'});
 
 // find all the Posts and iterate over them
 var outputJson = new Array();
-Blog.view('/blog/_design/Post/_view/by_created_at', {}, function(err, posts) {
-  if (err) throw err;
-  //jsonFile.write('[');
-  async.forEachSeries(posts.rows,function(item,cb){
-    Blog.get(item.id,function(err,doc) {
-      if (err) throw err;
-      //jsonFile.write( '\n\t' + JSON.stringify(doc) + ',' );
-      outputJson.push(doc);
-      logger.info('post [added]: ' + doc.title);
-    });
-    cb();
-  },function(err) {
+
+function buildJsonOutput(){
+  Blog.view('/blog/_design/Post/_view/by_created_at', {}, function(err, posts) {
     if (err) throw err;
-    //jsonFile.write('\n]');
-    logger.info('finished writing to file');
+    //jsonFile.write('[');
+    async.forEachSeries(posts.rows,function(item,cb){
+      Blog.get(item.id,function(err,doc) {
+        if (err) throw err;
+        outputJson.push(doc);
+        logger.info('post [added]: ' + doc.title);
+      });
+    },function(err) {
+      if (err) {
+        logger.error(u.inpect(err));
+        throw err;
+      }
+    });
   });
-  events.on("end",function(stream){
-    jsonFile.write(JSON.stringify(outputJson));
-  });
-});
+}
+
+function writeJsonFile(){
+  jsonFile.write(JSON.stringify(outputJson));
+}
+
+async.series([buildJsonOutput,writeJsonFile]);
