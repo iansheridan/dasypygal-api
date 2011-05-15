6a427a073b3efbb599ac31fa4379e87e387be6c9
Ian Sheridan <ian.sheridan@gmail.com>
2011-05-14 22:19:10 -0400

    final version of the actuall data extraction

--- write-posts-to-file.js
+++ write-posts-to-file.js
@@ -1,13 +1,12 @@
 var CouchClient = require('couch-client')
 var fs = require('fs');
-var asyncOne = require('async');
-var asyncTwo = require('async');
+var async = require('async');
 var u = require('util');
 var logger = require('logger').create();
 logger.level = 1;
 
 // open connection to the CouchDB Blog database
-var Blog = CouchClient("http://127.0.0.1:5984/blog");
+var Blog = CouchClient("http://dasypygal.dyndns.org:5984/blog");
 
 // open a file to write to
 var jsonFile = fs.createWriteStream('posts.json', {'flags': 'w'});
@@ -15,30 +14,22 @@ var jsonFile = fs.createWriteStream('posts.json', {'flags': 'w'});
 // find all the Posts and iterate over them
 var outputJson = new Array();
 
-asyncOne.series({
-  buildJsonPutput: function(callback){
-    var queue = new Array();
-    Blog.view('/blog/_design/Post/_view/by_created_at', {}, function(err, posts) {
-      if (err) throw err;
-      posts.rows.forEach(function(item){
-        queue.push(Blog.get(item.id,function(err,doc) {
-          if (err) throw err;
-          outputJson.push(doc);
-          logger.info('post [added]: ' + doc.title);
-        }));
-      });
-      queue.push(logger.info('[ONE] outputJson.length='+outputJson.length));
+var queue = new Array();
+Blog.view('/blog/_design/Post/_view/by_created_at', function(err, posts) {
+  if (err) throw err;
+  async.forEachSeries(posts.rows,function(item,callback){
+    Blog.get(item.id,function(err,doc) {
+      if (err) return callback(err);
+      outputJson.push(doc);
+      logger.info('post [added]: ' + doc.title);
     });
-    asyncTwo.series(queue);
-    callback(null, 'buildJsonPutput');
-  },
-  writeJson: function(callback){
+    callback(null);
+  },function(err){
+    if (err) throw err;
     setTimeout(function(){
       jsonFile.write(JSON.stringify(outputJson));
       logger.info('[TWO] outputJson.length='+outputJson.length);
-    },1500)
-    callback(null, 'writeJson');
-  }
-},function(err, results){
-  logger.info(u.inspect(results));
+    },10000);
+  });
 });
+
